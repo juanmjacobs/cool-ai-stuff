5ac882f88c44f0eaa650888b9de74f503b6a0ad9
Juan Jacobs <juanmjacobs@gmail.com>
2021-04-13 00:12:22 -0500

    face bound

--- maze.js
+++ maze.js
@@ -32,6 +32,7 @@ function refreshface() {
         draw(this, context,640,480);
     },false);
     let relativeOrigin;
+    let firstFace;
     async function draw(video,context, width, height)
     {
         //context.drawImage(video,0,0,width,height);
@@ -43,10 +44,13 @@ function refreshface() {
         const predictions = await model.estimateFaces(video, returnTensors);
           if (predictions.length > 0)
           {
+            firstFace = !relativeOrigin;
            //console.log(predictions);
-           for (let i = 0; i < predictions.length; i++) {
+           for (let i = 0; i < 1/*predictions.length*/; i++) {
              const start = predictions[i].topLeft;
-             if(!relativeOrigin) relativeOrigin = [640-start[0], start[1]];
+             if(firstFace) {
+                relativeOrigin = [640-start[0], start[1]];
+             }
              const end = predictions[i].bottomRight;
              var probability = predictions[i].probability;
              const size = [end[0] - start[0], end[1] - start[1]];
@@ -72,14 +76,21 @@ function refreshface() {
             try {
               const [ x, y ] = start;
               const [ xOrig, yOrig ] = relativeOrigin;
-              let faceX = width - x;
-              let faceY = y;
+              const movingFactor = firstFace ? 1 : 1.5; //moverse 1.5 veces mas que la cara? offset de cara anterior?
+              const faceSize = 50;
+              let faceX = movingFactor * (width - x);
+              let faceY = movingFactor * y;
 
               let faceXOrig = faceX - xOrig;
               if(faceXOrig < 0) faceXOrig = 0;
+              if(faceXOrig > width - faceSize) faceXOrig = width - faceSize;
               let faceYOrig = faceY - yOrig;
+              console.log({faceYOrig, height})
               if(faceYOrig < 0) faceYOrig = 0;
-              context.drawImage(face, faceXOrig , faceYOrig, 50, 50)
+              if(faceYOrig > height - faceSize) faceXOrig = height - faceSize;
+              
+              context.drawImage(face, faceXOrig , faceYOrig, faceSize, faceSize)
+
             } catch(e) {
               console.log("Error downloading face", face.src)
             }
