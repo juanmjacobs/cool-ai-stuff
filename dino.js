e61bef1a964cd3472e050a4b37c9ef5bf2f99421
Juan Jacobs <juanmjacobs@gmail.com>
2021-04-13 12:08:31 -0500

    chromedino

--- dino.js
+++ dino.js
@@ -54,54 +54,33 @@ function refreshface() {
     },false);
     let relativeOrigin;
     let firstFace;
-    let lastFaceOrigPosition;
+    let previousY;
     async function draw(video,context, width, height)
     {
-        //context.drawImage(video,0,0,width,height);
         context.clearRect(0, 0, width, height);
+        context.drawImage(video,0,0,width,height);        
 
         if(!model) model = await blazeface.load();
         const returnTensors = false;
         const predictions = await model.estimateFaces(video, returnTensors);
-          if (predictions.length > 0)
-          {
-            firstFace = !relativeOrigin;
-           //console.log(predictions);
-           for (let i = 0; i < 1/*predictions.length*/; i++) {
-             const start = predictions[i].topLeft;
-             if(firstFace) {
-                relativeOrigin = [640-start[0], start[1]];
-             }
-             const end = predictions[i].bottomRight;
-             var probability = predictions[i].probability;
-             const size = [end[0] - start[0], end[1] - start[1]];
-             // Render a rectangle over each detected face.
-             /*context.beginPath();
-             context.strokeStyle="green";
-             context.lineWidth = "4";
-             context.rect(start[0], start[1],size[0], size[1]);
-             context.stroke();
-             var prob = (probability[0]*100).toPrecision(5).toString();
-             var text = prob+"%";
-             context.fillStyle = "red";
-             context.font = "13pt sans-serif";
-             context.fillText(text,start[0]+5,start[1]+20);*/
-
-            //context.drawImage(document.getElementById('gardel'), 33, 71, 104, 124, 21, 20, 87, 104);
-            //context.drawImage(document.getElementById('gardel'), start[0], start[1],size[0], size[1], 21, 20, 87, 104);
-             context.strokeStyle="green";
-             context.lineWidth = "4";
-             context.stroke();
-            try {
+        if (predictions.length > 0) {
+           for (let i = 0; i < 1; i++) {
+              const start = predictions[i].topLeft;
+              const end = predictions[i].bottomRight;
+              var probability = predictions[i].probability;
+              const size = [end[0] - start[0], end[1] - start[1]];
               const [ x, y ] = start;
-              console.log("y",y)
-
-            } catch(e) {
-              console.log("Error downloading face", face.src)
-            }
+              const hasJumped = previousY - y > 10
+              console.log("y",y, "previousY", previousY, hasJumped? "JUMP!!!!!":"");
+              if(hasJumped) {
+                jump();
+                context.font = "100px Arial";
+                context.fillText("JUMP!!!", width/4, 200);
+              }
+              previousY = y
           }
-         }
-        setTimeout(draw,250,video,context,width,height);
+        }
+        setTimeout(draw,50,video,context,width,height);
     }
 })();
 
